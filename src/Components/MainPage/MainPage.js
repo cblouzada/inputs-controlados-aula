d95f7b64e4aaabf7982ab8cb77fbccc564d40eab
cblouzada <minhavidaemfloripa@hotmail.com>
2022-10-18 11:41:01 -0300

    Aula de inputs controlados

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,24 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {MainContainer, Form, Input} from './styles'
 
 function MainPage() {
+
+  const [Nome, setNome] = useState("")
+  const [Idade, setIdade] = useState("")
+  const [Email, setEmail] = useState("")
+
+  const onChangeName = (event) =>{
+    // console.log("Mudei o input nome")
+    // console.log(event.target.value)
+    setNome(event.target.value)
+  }
+  const onChangeIdade = (event) =>{
+    // console.log("Mudei o input Idade")
+    // console.log(event.target.value)
+    setIdade(event.target.value)
+  }
+  const onChangeEmail = (event) =>{
+    // console.log("Mudei o input Email")
+    // console.log(event.target.value)
+    setEmail(event.target.value)
+  }
+
+  const enviarDados = () =>{
+
+     ("Nome, Idade, Email");
+
+  setNome("")
+  setIdade("")
+  setEmail("")
+}
+
+
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
       <Form>
         <label>
           Nome:
-          <Input />
+          <Input onChange={onChangeName} type="text" placeholder='Digite seu nome' value={Nome} />
         </label>
         <label>
           Idade:
-          <Input/>
+          <Input onChange={onChangeIdade} type='number' placeholder='Digite sua idade' value={Idade}/>
         </label>
         <label>
           E-mail:
-          <Input />
+          <Input onChange={onChangeEmail} type="email" placeholder='Digite seu email' value={Email}/>
         </label>
-      <button>Enviar dados</button>
+      <button onClick={enviarDados}>Enviar dados</button>
       </Form>
     </MainContainer>
   )
